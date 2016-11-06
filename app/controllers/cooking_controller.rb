343ede905823c6eb5cec6f3b83cc7875d04980aa
Alessandra <alessandra.m.vasquez@gmail.com>
2016-11-06 18:43:30 -0500

    center and bootstrap

--- app/controllers/cooking_controller.rb
+++ app/controllers/cooking_controller.rb
@@ -2,7 +2,7 @@ class CookingController < ApplicationController
   def index
     user_input = params["food"]
     if user_input
-      url = "https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UCJFp8uSYCjXOMnkUyb3CQ3Q&maxResults=5&order=relevance&q=#{user_input}&key=#{ENV['YOUTUBE_TASTY_ID']}"
+      url = "https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UCJFp8uSYCjXOMnkUyb3CQ3Q&maxResults=4&order=relevance&q=#{user_input}&key=#{ENV['YOUTUBE_TASTY_ID']}"
       @videoinfo = HTTParty.get(url)
       @video = @videoinfo["items"]
     end
